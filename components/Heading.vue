719f312dccdbde25edd84212a4d7a41247f6dc9c
Aaron Choo <80137634+aaron-choo@users.noreply.github.com>
2022-10-13 20:03:49 +0800

    feat: updated heading component

--- components/Heading.vue
+++ components/Heading.vue
@@ -5,7 +5,7 @@
     :class="{
       'text-5xl md:text-[5rem]': size === '8xl',
       'text-4xl md:text-6xl': size === '6xl',
-      'text-2xl': size === '2xl'
+      'text-2xl': size === '2xl',
     }"
   >
     <slot />
@@ -17,12 +17,28 @@ export default {
   props: {
     as: {
       type: [String, Object, Function],
-      default: 'h1'
+      default: "h1",
     },
     size: {
       type: String,
-      default: '8xl'
-    }
-  }
-}
+      default: "8xl",
+    },
+    as: {
+      type: [String, Object, Function],
+      default: "h2",
+    },
+    size: {
+      type: String,
+      default: "6xl",
+    },
+    as: {
+      type: [String, Object, Function],
+      default: "h3",
+    },
+    size: {
+      type: String,
+      default: "2xl",
+    },
+  },
+};
 </script>
