686d6e6362450929624698436bb79aa7d36406ea
Aaron Choo <aaronchoo@gmail.com>
2022-10-25 23:17:29 +0800

    feat: added background to bounded component

--- components/Bounded.vue
+++ components/Bounded.vue
@@ -2,11 +2,19 @@
   <Component
     :is="as"
     :data-collapsible="collapsible || null"
+    :style="
+      backgroundImage
+        ? 'background-image:url(' + backgroundImage.url + ');'
+        : null
+    "
     class="px-6"
     :class="{
+      'py-4': yPadding === 'xs',
       'py-8 md:py-10': yPadding === 'sm',
-      'py-20 md:py-32': yPadding === 'base',
+      'py-12 md:py-20': yPadding === 'base',
+      'py-20 md:py-32': yPadding === 'md',
       'secondary-background': secondaryBackground,
+      'background-image': backgroundImage,
     }"
   >
     <div class="mx-auto w-full max-w-7xl">
@@ -21,6 +29,10 @@ export default {
     as: {
       type: [String, Object, Function],
       default: "div",
+    },
+    backgroundImage: {
+      type: Object,
+      default: null,
     },
     yPadding: {
       type: String,
@@ -40,5 +52,12 @@ export default {
 <style scoped>
 .secondary-background {
   background: var(--bg-secondary);
+}
+.background-image {
+  color: var(--bg);
+  background-size: cover;
+  background-position: center;
+  background-color: var(--color);
+  background-blend-mode: overlay;
 }
 </style>
