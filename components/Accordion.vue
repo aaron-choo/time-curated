b1803b0b813fdf82e1b5ba2d35c534385c30b307
Aaron Choo <aaronchoo@gmail.com>
2022-10-29 04:25:33 +0800

    feat: added accordion component

---
+++ components/Accordion.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <button class="uppercase" @click="accordionOpen = !accordionOpen">
+      {{ label }}
+    </button>
+    <div
+      class="accordion w-full transition-all duration-500"
+      :class="{
+        'h-0 opacity-0 pointer-events-none': !accordionOpen,
+        'h-auto': accordionOpen,
+      }"
+    >
+      <Bounded as="div" yPadding="xs">
+        <ul class="flex flex-col gap-2 uppercase">
+          <template v-for="(link, index) in links">
+            <li v-if="index > 0" :key="link.label">
+              <PrismicLink :field="link.link">
+                {{ link.label }}
+              </PrismicLink>
+            </li>
+          </template>
+        </ul>
+      </Bounded>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ["label", "links"],
+  watch: {
+    $route(to, from) {
+      this.accordionOpen = false;
+    },
+  },
+  data() {
+    return {
+      accordionOpen: false,
+    };
+  },
+};
+</script>
+<style scoped>
+</style>
