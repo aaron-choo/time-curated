6d61fb75734d37d5b81d9b876c509bb68d037af7
Aaron Choo <aaronchoo@gmail.com>
2022-11-06 22:43:29 +0800

    feat: added snipcart api

--- pages/shop/_category/_uid.vue
+++ pages/shop/_category/_uid.vue
@@ -6,79 +6,7 @@
           <ProductDetailImage :product="product" :variant="variant" />
         </div>
         <div class="md:col-span-5">
-          <div class="product-summary mb-4">
-            <Heading as="h1" size="2xl" class="product-title my-0">
-              {{ product.title }}
-            </Heading>
-            <Heading as="p" size="lg" class="product-price my-2">
-              <template v-if="product.sale_price">
-                <span v-if="variant" class="flex gap-2 items-center">
-                  <span
-                    class="original-price opacity-50 line-through decoration-1"
-                    >SGD {{ (product.price + variant.price).toFixed(2) }}</span
-                  >
-                  <span class="sale-price"
-                    >SGD
-                    {{ (product.sale_price + variant.price).toFixed(2) }}</span
-                  >
-                  <span
-                    class="
-                      sale-percentage
-                      h-fit
-                      font-sans
-                      text-xs
-                      leading-none
-                      py-1
-                      px-2
-                      rounded-[3px]
-                    "
-                  >
-                    {{
-                      Math.round(
-                        (1 -
-                          (product.sale_price + variant.price) /
-                            (product.price + variant.price)) *
-                          100
-                      )
-                    }}% Off</span
-                  >
-                </span>
-                <span v-else class="flex gap-2 items-center">
-                  <span
-                    class="original-price opacity-50 line-through decoration-1"
-                    >SGD {{ product.price.toFixed(2) }}</span
-                  >
-                  <span class="sale-price"
-                    >SGD {{ product.sale_price.toFixed(2) }}</span
-                  >
-                  <span
-                    class="
-                      sale-percentage
-                      h-fit
-                      font-sans
-                      text-xs
-                      leading-none
-                      py-1
-                      px-2
-                      rounded-[3px]
-                    "
-                  >
-                    {{
-                      Math.round(
-                        (1 - product.sale_price / product.price) * 100
-                      )
-                    }}% Off</span
-                  >
-                </span>
-              </template>
-              <template v-else>
-                <span v-if="variant">
-                  SGD {{ (product.price + variant.price).toFixed(2) }}
-                </span>
-                <span v-else> SGD {{ product.price.toFixed(2) }} </span>
-              </template>
-            </Heading>
-          </div>
+          <ProductSummary :product="product" :variant="variant" />
           <div
             class="
               add-to-cart-section
@@ -232,6 +160,19 @@ export default {
     //   "https://api.apilayer.com/exchangerates_data/latest?base=SGD"
     // );
     // console.log(currencyRates);
+
+    $axios.setHeader(
+      "Authorization",
+      "Basic " +
+        Buffer.from(process.env.NUXT_ENV_SNIPCART_SECRET_API_KEY).toString(
+          "base64"
+        )
+    );
+    $axios.setHeader("Accept", "application/json");
+    const productInfo = await $axios.$get(
+      `https://app.snipcart.com/api/products/${params.uid}/`
+    );
+    console.log(productInfo);
 
     if (page.data.product_category.uid === params.category) {
       return {
