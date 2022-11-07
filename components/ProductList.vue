f454ec31c6f43778287942ad12469b34f5db782a
Aaron Choo <aaronchoo@gmail.com>
2022-11-07 15:20:50 +0800

    feat: updated ecommerce

--- components/ProductList.vue
+++ components/ProductList.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-    <div v-for="product in products" :key="product.id">
+    <div v-for="product in products" :key="product.uid">
       <nuxt-link :to="product.url">
         <product-view :product="product"></product-view
       ></nuxt-link>
