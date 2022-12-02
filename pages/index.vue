85196d154140a1ba3dc943d7e18d26a56b85bc0f
Aaron Choo <aaronchoo@gmail.com>
2022-12-02 22:43:34 +0800

    feat: updated home page

--- pages/index.vue
+++ pages/index.vue
@@ -27,7 +27,38 @@ export default {
   },
   head() {
     return {
-      title: this.page.data.title,
+      title: `${this.$prismic.asText(
+        this.$store.state.prismic.settings.data.siteTitle
+      )}`,
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: this.$prismic.asText(this.page.data.description),
+        },
+        {
+          hid: "keywords",
+          name: "keywords",
+          content: [this.page.data.title, this.page.data.keywords].join(", "),
+        },
+        {
+          hid: "og-title",
+          property: "og:title",
+          content: `${this.$prismic.asText(
+            this.$store.state.prismic.settings.data.siteTitle
+          )}`,
+        },
+        {
+          hid: "og-desc",
+          property: "og:description",
+          content: this.$prismic.asText(this.page.data.description),
+        },
+        {
+          hid: "og-url",
+          property: "og:url",
+          content: "https://time-curated.com" + this.page.url,
+        },
+      ],
     };
   },
 };
